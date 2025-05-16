aadc2c0282b616bebf4be2db8ddf44a1caee5bea
darling-emma <emma@darling.nyc>
2025-05-16 12:18:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("connected - addback-pinsubhero");
+console.log("connected - removecanvascode-test");
 
 // Register Plugins
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -100,138 +100,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         markers: false,
     });
 
-    // SUBHERO ANIMATION
-    const imageURLs = [
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321960/01_wmfw04.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321960/02_t4y3dq.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321960/03_rqpwid.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321961/04_wtwh9j.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321961/05_nn5zst.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321961/06_mtehvs.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321961/07_xkfv9t.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321961/08_izkkbi.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321962/09_g7qdnb.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321961/10_u2arir.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321962/11_ca579c.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321962/12_eq57wt.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321962/13_rdu0zf.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321962/14_usjip1.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321963/15_jgdn4l.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321963/16_xvutpd.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321963/17_lwmuez.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321963/18_lrz2ld.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321964/19_r6mhad.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321964/20_bb7czi.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321964/21_pifxub.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321964/22_lj1nhw.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321965/23_xwo9bb.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321965/24_xsyc7x.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321965/25_uwn4p5.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321966/26_k5nfb7.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321966/27_rsovjs.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321966/28_ifjfkn.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321966/29_up8jh8.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321967/30_pwvsju.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321967/31_jiykee.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321967/32_kmoruf.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321968/33_gpvl8v.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321968/34_qrvmam.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321968/35_p03hd3.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321969/36_a8o4j4.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321969/37_jret6y.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321969/38_pe6qem.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321970/39_emcscw.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747321970/40_mqam03.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747322261/41_tylms7.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747322262/42_qwjoao.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747322264/43_f8gss0.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747322254/44_hq3xos.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747322256/45_al18pr.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747322257/46_ztcdx3.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747322259/47_eijxun.webp",
-        "https://res.cloudinary.com/dwxgs5mgk/image/upload/v1747322260/48_bqiauq.webp"
-    ];
-
-    const frameCount = imageURLs.length;
-    const canvas = document.getElementById("sequence-canvas");
-    const context = canvas.getContext("2d");
-
-    const images = [];
-    const imageSeq = {
-        frame: 0
-    };
-
-    function preloadImages() {
-        imageURLs.forEach((url) => {
-            const img = new Image();
-            img.src = url;
-            images.push(img);
-        });
-    }
-
-    function render() {
-        const img = images[imageSeq.frame];
-        if (!img?.complete) return;
-
-        const container = document.querySelector(".canvas-window");
-        const displayWidth = container.clientWidth;
-        const displayHeight = container.clientHeight;
-
-        const canvasAspect = displayWidth / displayHeight;
-        const imgAspect = img.width / img.height;
-
-        let drawWidth, drawHeight;
-
-        if (imgAspect > canvasAspect) {
-            drawHeight = displayHeight;
-            drawWidth = drawHeight * imgAspect;
-        } else {
-            drawWidth = displayWidth;
-            drawHeight = drawWidth / imgAspect;
-        }
-
-        const x = (displayWidth - drawWidth) / 2;
-        const y = (displayHeight - drawHeight) / 2;
-
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(img, x, y, drawWidth, drawHeight);
-    }
-
-    function resizeCanvas() {
-        const container = document.querySelector(".canvas-window");
-        const width = container.clientWidth;
-        const height = container.clientHeight;
-
-        canvas.style.width = width + 'px';
-        canvas.style.height = height + 'px';
-        canvas.width = width;
-        canvas.height = height;
-
-        render();
-    }
-
-    function resizeCanvas() {
-        const container = document.querySelector(".canvas-window");
-        const width = container.clientWidth;
-        const height = container.clientHeight;
-
-        // Match display size
-        canvas.style.width = width + "px";
-        canvas.style.height = height + "px";
-
-        // Match pixel buffer for sharpness
-        canvas.width = width;
-        canvas.height = height;
-
-        render();
-    }
-
-    window.addEventListener("resize", () => {
-        resizeCanvas();
-    });
-
-    preloadImages();
-    resizeCanvas();
 
     const subheroTimeline = gsap.timeline({
         scrollTrigger: {
@@ -245,12 +113,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 
     subheroTimeline
-    .to(imageSeq, {
-        frame: frameCount - 1,
-        snap: "frame",
-        ease: "none",
-        onUpdate: render
-    })
     .to(".scramble",{
         y: -40,
         opacity: 0
